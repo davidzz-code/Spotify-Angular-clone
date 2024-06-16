b7b134ce31c55b5cec24cc459b4b2a287ec67ef6
David Ramírez <rrdavid08@gmail.com>
2024-06-16 19:49:56 +0200

    feat: Add lazy loading for home module

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    HomePageComponent,
   ]
 })
 export class HomeModule { }
