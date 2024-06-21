bd339aef97ad138241e9fd1061aa15e5bfb1d937
David Ramírez <rrdavid08@gmail.com>
2024-06-21 18:16:40 +0200

    feat: Improve login page UI and functionality

--- src/app/modules/auth/pages/login-page/login-page.component.ts
+++ src/app/modules/auth/pages/login-page/login-page.component.ts
@@ -1,12 +1,37 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
 })
 export class LoginPageComponent {
+  formLogin: FormGroup = new FormGroup({});
 
+  constructor() { }
+
+  ngOnInit(): void {
+    this.formLogin = new FormGroup(
+      {
+        email: new FormControl('', [
+          Validators.required,
+          Validators.email
+        ]),
+        password: new FormControl('', [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(20),
+        ]),
+      }
+    )
+  }
+
+  sendLogin(): void {
+    const body = this.formLogin.value;
+    console.log(body)
+  }
 }
