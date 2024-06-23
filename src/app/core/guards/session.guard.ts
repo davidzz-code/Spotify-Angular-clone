f3704f4f78ae152e3375b31dc897978c4871d35d
David Ramírez <rrdavid08@gmail.com>
2024-06-23 18:45:22 +0200

    feat: Add session guard to home page route

---
+++ src/app/core/guards/session.guard.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SessionGuard implements CanActivate {
+
+  constructor(private cookieService: CookieService, private router: Router){}
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot ) {
+    return this.checkCookieSession();
+  };
+
+  checkCookieSession(): boolean {
+    try {
+      const token: boolean = this.cookieService.check('token');
+      console.log('TODO OK, muestrame:', token)
+      if (!token) this.router.navigate(['/', 'auth'])
+
+      return token;
+    } catch (error) {
+      console.log('Algo pasó');
+      return false;
+    }
+  }
+}
